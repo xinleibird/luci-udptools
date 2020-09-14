1685c64f9980599422c69fec9cb0347fa5e19c9b
Xin Lei <xinleibird@gmail.com>
2020-09-14 21:16:52 +0800

    Update udptools.lua

--- src/luci/controller/udptools.lua
+++ src/luci/controller/udptools.lua
@@ -1,11 +1,11 @@
 module("luci.controller.udptools", package.seeall)
 
 function index()
-	entry({"admin", "network", "udptools"}, firstchild(), _("UDPTools")).dependent = false
-	entry({"admin", "network", "udptools", "config"}, cbi("udptools"), _("Config"), 2)
-	entry({"admin", "network", "udptools", "control"}, template("control"), _("Control"), 1)
-	entry({"admin", "network", "udptools", "restart"}, call("restart"))
-	entry({"admin", "network", "udptools", "stop"}, call("stop"))
+	entry({"admin", "services", "udptools"}, firstchild(), _("UDPTools")).dependent = false
+	entry({"admin", "services", "udptools", "config"}, cbi("udptools"), _("Config"), 2)
+	entry({"admin", "services", "udptools", "control"}, template("control"), _("Control"), 1)
+	entry({"admin", "services", "udptools", "restart"}, call("restart"))
+	entry({"admin", "services", "udptools", "stop"}, call("stop"))
 end
 
 function restart()
